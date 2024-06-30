d1fc9ce43fa2c835fde88d445ac9dfb6b9eef9e1
SinisterSpatula <sinisterspatula@gmail.com>
2024-06-30 18:18:47 -0500

    update

--- nodebb-plugin-avatargallery/lib/admin.js
+++ nodebb-plugin-avatargallery/lib/admin.js
@@ -6,21 +6,26 @@ define('admin/plugins/avatargallery', ['api'], function (api) {
   AvatarGallery.init = function () {
     $('#submit-avatar').on('click', function () {
       // Handle form submission
+      console.log('Submitting avatar');
     });
 
     $('.rename-avatar').on('click', function () {
       var avatarId = $(this).data('id');
       // Handle renaming
+      console.log('Renaming avatar ' + avatarId);
     });
 
     $('.delete-avatar').on('click', function () {
       var avatarId = $(this).data('id');
       // Handle deletion
+      console.log('Deleting avatar ' + avatarId);
     });
 
     $('#avatar-search').on('input', function () {
       // Handle search/filtering
+      console.log('Searching for ' + $(this).val());
     });
+    console.log('Avatar Gallery loaded admin panel script initialized!');
   };
 
   return AvatarGallery;
